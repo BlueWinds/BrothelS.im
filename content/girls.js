134bbba0c123bff6b0b45d19fee88494d04a4540
BlueWinds <bs.im@everblue.info>
2012-12-09 23:09:27 -0800

    Added Dungeon info to Obedience tooltip

--- content/girls.js
+++ content/girls.js
@@ -16,7 +16,7 @@ define({
   tooltips: {
     endurance: "Endurance is how tired the girl currently is. Making her work when her endurance is low will decrease happiness, and she'll also be less effective.",
     happiness: "Happiness is how much she enjoys her job. This goes down any time she refuses to do a task, or when you're not paying her enough. Increase it by paying her more and giving her time off - she's more likely to refuse when unhappy.",
-    obedience: "Obedience is how likely she is to comply when you ask her to do tasks some might find distasteful or dangerous. This decreases whenever she refuses.",
+    obedience: "Obedience is how likely she is to comply when you ask her to do tasks some might find distasteful or dangerous. This decreases whenever she refuses. You can increase it using the Lockdown action, available once you own a building and have purchased a Dungeon.",
     charisma: "Charisma is how outgoing, personable and generally attractive she is. Girls with higher charisma get paid more for prostitution.",
     intelligence: "Intelligence is how quickly she learns. Girls with higher intelligence will gain experience and levels more quickly.",
     constitution: "Constitution is a measure of how quickly she tires. A higher constitution means that Endurance will fall more slowly and recover more quickly.",
