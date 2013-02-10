19bc3829d060ae78d96bbe2adcb80cbccc16dc47
Peter Frauenglass <github.com@everblue.info>
2013-02-10 13:06:27 -0800

    Relative path fix for wiki.js

--- modules/validator/wiki.js
+++ modules/validator/wiki.js
@@ -2,19 +2,21 @@
 /* Any JavaScript here will be loaded for all users on every page load. */
 var gameSchemas;
 var Schemas = window.Schemas || {};
-$(function() {
+initSchema = function(loadPath) {
   "use strict";
+  loadPath = loadPath || '';
   var scripts = ["game.js", "girls.js", "messages.js", "buildings.js", "resolvable.js", "missions.js", "actions.js", "events.js"];
   var series = [];
   series.push(function(next) {
-    $.getScript('../jSunny/jSunny.js', next);
+    $('head').append('<link href="' + loadPath + 'libraries/jSunny/jSunny.css" type="text/css" rel="stylesheet">');
+    $.getScript(loadPath + 'libraries/jSunny/jSunny.js', next);
   });
   scripts.forEach(function(script) {
     series.push(function(next) {
-      $.getScript('modules/validator/' + script, next);
+      $.getScript(loadPath + 'modules/validator/' + script, next);
     });
   });
-  $.getScript('modules/core.js', function() {
+  $.getScript(loadPath + 'modules/core.js', function() {
     e.runSeries(series, function() {
       gameSchemas = new Schema(Schemas.Game);
       $.each(Schemas, function(type, schema) {
@@ -22,4 +24,4 @@ $(function() {
       });
     });
   });
-});
+};
