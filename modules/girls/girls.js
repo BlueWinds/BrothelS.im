cf80037ff39c5c3880e053d0e10752e04219aa2b
BlueWinds <sbx@everblue.info>
2013-02-25 09:26:38 -0800

    Fixed multiple-girl selections

--- modules/girls/girls.js
+++ modules/girls/girls.js
@@ -121,39 +121,43 @@ e.GameRender.push(function(done) {
       girl: girl,
       girls: g.girls._filter('status', 'Hired')
     };
-    var view = $(ejs.render($('#girls_view_template').html(), context).trim());
+    var mainView = $(ejs.render($('#girls_view_template').html(), context).trim());
 
-    var desired = girl.desiredPay();
-    var happinessDelta = $('#pay .delta.happiness', view);
-    var moneyDelta = $('#pay .delta.money', view);
-    var slider = $('#pay .slider', view).slider({
-      step: 0.01,
-      min: Math.min.apply(undefined, Object.keys(Girl.config.pay)),
-      max: Math.max.apply(undefined, Object.keys(Girl.config.pay)),
-      slide: function(event, ui) {
-        var closest = 100;
-        for (var mult in Girl.config.pay) {
-          closest = Math.abs(ui.value - mult) < Math.abs(ui.value - closest) ? mult : closest;
+    $('.girl-view', mainView).each(function() {
+      var view = $(this);
+      var girl = g.girls[view.attr('name')];
+      var desired = girl.desiredPay();
+      var happinessDelta = $('.pay .delta.happiness', view);
+      var moneyDelta = $('.pay .delta.money', view);
+      var slider = $('.pay .slider', view).slider({
+        step: 0.01,
+        min: Math.min.apply(undefined, Object.keys(Girl.config.pay)),
+        max: Math.max.apply(undefined, Object.keys(Girl.config.pay)),
+        slide: function(event, ui) {
+          var closest = 100;
+          for (var mult in Girl.config.pay) {
+            closest = Math.abs(ui.value - mult) < Math.abs(ui.value - closest) ? mult : closest;
+          }
+          if (girl.actions.pay != closest) {
+            girl.actions.pay = parseFloat(closest);
+            var happiness = girl.payHappiness();
+            happinessDelta.html(happiness < 0 ? happiness : '+' + happiness);
+            moneyDelta.html('$' + Math.floor(desired * girl.actions.pay));
+            slider.slider('value', closest);
+          }
+          event.preventDefault();
         }
-        if (girl.actions.pay != closest) {
-          girl.actions.pay = parseFloat(closest);
-          var happiness = girl.payHappiness();
-          happinessDelta.html(happiness < 0 ? happiness : '+' + happiness);
-          moneyDelta.html('$' + Math.floor(desired * girl.actions.pay));
-          slider.slider('value', closest);
-        }
-        event.preventDefault();
-      }
-    }).slider('value', girl.actions.pay);
+      }).slider('value', girl.actions.pay);
 
-    $('.checkbox', view).click(function() {
-      var check = !$(this).hasClass('checked');
-      var sex = $(this).attr('id');
-      if (check) {
-        girl.actions[sex] = true;
-      } else {
-        delete girl.actions[sex];
-      }
+      $('.checkbox', view).click(function() {
+        var check = !$(this).hasClass('checked');
+        var sex = $(this).attr('id');
+        if (check) {
+          girl.actions[sex] = true;
+        } else {
+          delete girl.actions[sex];
+        }
+      });
     });
 
     var opt = {
@@ -161,8 +165,8 @@ e.GameRender.push(function(done) {
         g.render();
       }
     };
-    view.dialog(opt);
-    view.closest('.ui-dialog').addClass('tab-dialog');
+    mainView.dialog(opt);
+    mainView.closest('.ui-dialog').addClass('tab-dialog');
   });
 
   $('#hire-girl').click(function() {
