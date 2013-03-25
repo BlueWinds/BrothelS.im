60f38b808a610ac9b44e03d25435092bd087f5c3
OrangeGucci415 <orangegucci415@gmail.com>
2013-03-25 08:33:06 -0700

    Update explore.js

--- content/actions/explore.js
+++ content/actions/explore.js
@@ -14,7 +14,7 @@ Actions.Explore = {
     }
   },
   options: {
-    Redlight: 'Redlight', //:"The redlight district. Though relatively small, it's quite busy since sex is the nation's primary product.",
+    Redlight: 'Redlight', //:"The red-light district. Though relatively small, it\'s quite busy since sex is the nation\'s primary product.",
     Slums: 'Slums', //: "There are better parts of town, but none more populous or unwatched by the law.",
     Docks: 'Docks', //: "As the primary port of an island nation, the port never sleeps.",
     Market: 'Market', //: "Anything can be bought or sold in the central market - if not in broad daylight, then at least in one of the smaller, nondescript buildings nearby.",
@@ -37,7 +37,7 @@ Actions.Explore = {
         group: '<<- girl.name >>',
         label: 'Explore Redlight District',
         image: 'content/miscImages/redlightMorning.jpg',
-        text: 'You and <<- girl.name >> wander through the streets of the redlight district, looking for something interesting. While there\'s plenty aimed to catch your eye, most of the attractions are closed in the morning. You may have better luck in the evening.'
+        text: 'You and <<- girl.name >> wander through the streets of the red-light district, looking for something interesting. While there\'s plenty aimed to catch your eye, most of the attractions are closed in the morning. You may have better luck in the evening.'
       },
       girl: {
         endurance: 3
@@ -59,7 +59,7 @@ Actions.Explore = {
         group: '<<- girl.name >>',
         label: 'Explore Slums',
         image: 'content/miscImages/slumsMorning.jpg',
-        text: "The slums don't look so bad in the morning light. Nearly three quarters of the population live and work in this part of the city, afterall, so it can't be nearly as bad as its reputation suggests. You and <<- girl.name >> spend some time hunting for bargains at smaller, out of the way markets. Once your nose gets used to it, it's almost a pleasant place."
+        text: "The slums don\'t look so bad in the morning light. Nearly three-quarters of the population lives and works in this part of the city, afterall, so it can\'t be nearly as bad as its reputation suggests. You and <<- girl.name >> spend some time hunting for bargains at smaller, out of the way markets. Once your nose gets used to it, it\'s almost a pleasant place."
       },
       girl: {
         endurance: -6
@@ -70,7 +70,7 @@ Actions.Explore = {
         group: '<<- girl.name >>',
         label: 'Explore Slums',
         image: 'content/miscImages/slumsEvening.jpg',
-        text: "<<- girl.name >> sticks close to your side as you venture into poor parts of the city. Streetlights are spaced much further than in other areas, and the ever-present city watch is hard to find. The slums aren't quiet, though - there's plenty of drinking and merry-making going on. You stick close to the pubs and away from dark alleys, and nothing untoward happens during your visit."
+        text: "<<- girl.name >> sticks close to your side as you venture into poor parts of the city. Street lights are spaced much further than in other areas, and the ever-present city watch is hard to find. The slums aren\'t quiet, though - there\'s plenty of drinking and merry-making going on. You stick close to the pubs and away from dark alleys, and nothing untoward happens during your visit."
       },
       girl: {
         endurance: -6,
@@ -82,7 +82,7 @@ Actions.Explore = {
         group: '<<- girl.name >>',
         label: 'Exlore Docks',
         image: 'content/miscImages/docksMorning.jpg',
-        text: 'The docks are divided into two sections. You explore the larger, larger southern wharves this morning - they deal with great sea-going vessels and small fishing boats alike, and if you have time to spare, are an excellent place to take a sneak peak at goods before they arrive in the market. Despite several hours of searching however, you and <<- girl.name >> don\'t find anything particularly worth the effort of walking so far.'
+        text: 'The docks are divided into two sections. You explore the larger southern wharves this morning - they deal with great sea-going vessels and small fishing boats alike, and if you have time to spare, are an excellent place to take a sneak peak at goods before they arrive in the market. Despite several hours of searching however, you and <<- girl.name >> don\'t find anything particularly worth the effort of walking so far.'
       },
       girl: {
         endurance: -9
@@ -104,7 +104,7 @@ Actions.Explore = {
         group: '<<- girl.name >>',
         label: 'Explore Market',
         image: 'content/miscImages/marketMorning.jpg',
-        text: "The market is a fine place to explore in the morning - bustling, lively, full of the populace going about their daily business. Though famous as a sex-capital, the vast majority of people have nothing to do with the oldest profession. Shoes still need to be made, bread baked and iron bent and pounded into nails. You end up buying a sweet treat for both you and <<- girl.name >>, but nothing otherwise catches your attention."
+        text: "The market is a fine place to explore in the morning - bustling, lively, full of the populace going about their daily business. Though famous as a sex-capitol, the vast majority of people have nothing to do with the oldest profession. Shoes still need to be made, bread baked, and iron bent and pounded into nails. You end up buying a sweet treat for both you and <<- girl.name >>, but nothing otherwise catches your attention."
       },
       girl: {
         endurance: -6,
@@ -117,7 +117,7 @@ Actions.Explore = {
         group: '<<- girl.name >>',
         label: 'Explore Market',
         image: 'content/miscImages/marketEvening.jpg',
-        text: "The evening market has an aspect of a small-scale permanent fair, as the day's vegetable stands and candle-booths give way to sweat and savory treats, jewelry and perfumes, and, occasionally, the sort of goods that might be scandalous in a more reserved society. You catch <<- girl.name >> looking at a well-stocked stall full of dildos <<- girl.modesty < 50 ? 'hungrily' : 'out of the corner of her eye' >>, and ask if she'd like to stop in. She blushes and shakes her head."
+        text: "The evening market has an aspect of a small-scale permanent fair, as the day\'s vegetable stands and candle-booths give way to sweet and savory treats, jewelry and perfumes, and, occasionally, the sort of goods that might be scandalous in a more reserved society. You catch <<- girl.name >> looking at a well-stocked stall full of dildos <<- girl.modesty < 50 ? 'hungrily' : 'out of the corner of her eye' >>, and ask if she\'d like to stop in. She blushes and shakes her head."
       },
       girl: {
         endurance: -6
@@ -163,7 +163,7 @@ Actions.Explore = {
         group: '<<- girl.name >>',
         label: 'Explore University',
         image: 'content/miscImages/universityMorning.jpg',
-        text: "The university is rather quiet in the evening. Not many students live on campus, only those few who were allowed in on merit rather than family wealth or power. There are still a few evening courses, but only the library is anything like as busy as during the morning. <<- girl.name >> and you explore the area, but don't find much of interest."
+        text: "The university is rather quiet in the evening. Not many students live on campus, only those few who were allowed in on merit rather than family wealth or power. There are still a few evening courses, but only the library is anything like as busy as during the morning. <<- girl.name >> and you explore the area, but don\'t find much of interest."
       },
       girl: {
         endurance: -9
@@ -174,7 +174,7 @@ Actions.Explore = {
         group: '<<- girl.name >>',
         label: 'Explore Uptown',
         image: 'content/miscImages/uptownMorning.jpg',
-        text: "Uptown is a quiet place in the morning. Not sleepy, but unhurried. The wealthy with business to attend to have already left, while those with little to do are not nearly as boisterous as people in other parts of the city - there are appearances to maintain, afterall. <<- girl.name >> << if (girl.modesty < 50) { >>attracts some murmors and sidelong glances from a couple of noble ladies. You can't quite tell if they're condemning her revealing clothing or secretly envious of how good she looks.<< } else { >>fits in rather nicely with the surroundings, elegant and demure. No one would guess she works at a brothel.<< } >>"
+        text: "Uptown is a quiet place in the morning. Not sleepy, but unhurried. The wealthy with business to attend to have already left, while those with little to do are not nearly as boisterous as people in other parts of the city - there are appearances to maintain, afterall. <<- girl.name >> << if (girl.modesty < 50) { >>attracts some murmors and sidelong glances from a couple of noble ladies. You can't quite tell if they\'re condemning her revealing clothing or secretly envious of how good she looks.<< } else { >>fits in rather nicely with the surroundings, elegant and demure. No one would guess she works at a brothel.<< } >>"
       },
       girl: {
         endurance: -6,
@@ -210,7 +210,7 @@ Actions.Explore = {
         group: '<<- girl.name >>',
         label: 'Explore Garrison',
         image: 'content/miscImages/garrisonGate.jpg',
-        text: "The military and police force of the city has always seemed rather distant from the general populace - not hostile or aloof or unapproachable, just... different. You've never felt the difference quite as strongly as now, with the gate in front of you bolted and no one in sight. Though you feel as though you're being watched as you approach, it doesn't open."
+        text: "The military and police force of the city has always seemed rather distant from the general populace - not hostile or aloof or unapproachable, just... different. You\'ve never felt the difference quite as strongly as now, with the gate in front of you bolted and no one in sight. Though you feel as though you\'re being watched as you approach, it doesn\'t open."
       },
       girl: {
         endurance: -8,
