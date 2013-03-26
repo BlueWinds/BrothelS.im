3ef3eb3516a192c5ff46dbe21bebb1f849ad92f9
OrangeGucci415 <orangegucci415@gmail.com>
2013-03-26 08:51:40 -0700

    Consistency Fix
    Second editing pass

--- content/buildings/buildings.js
+++ content/buildings/buildings.js
@@ -12,7 +12,7 @@ Building.config = {
 };
 
 $.extend(Game.tooltips, {
-  clean: "Cleanliness is how orderly and generally lacking in grime the building is. Higher cleaning ratings make customers more likely to enter and pay well when they do, and low cleanliness will reduce girls\' endurance and happiness.",
+  clean: "Cleanliness is how orderly and generally lacking in grime the building is. Higher Clean ratings make customers more likely to enter and pay well when they do, and low cleanliness will reduce girls\' endurance and happiness.",
   reputation: "Reputation is a measure of how well a building is known, both as a place where adult entertainment can be found and for the quality of the girls usually found within. Higher reputation will attract better customers and more of them.",
   rooms: "Built / Total<br><br>Many buildings have extra rooms that could be renovated into extra bedrooms or other interesting places."
 });
